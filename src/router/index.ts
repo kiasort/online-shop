c32fc25546531dd324e562c9ea475ca662b79798
kiasort <amirowartem06@gmail.com>
2026-07-29 17:31:09 +0500

    feat: add vue-router with 6 lazy-loaded routes and view stubs

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,47 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import type { RouteRecordRaw } from 'vue-router'
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import ('../views/HomeView.vue')
+  },
+
+  {
+    path: '/catalog',
+    name: 'Catalog',
+    component: () => import ('../views/CatalogView.vue')
+  },
+
+  {
+    path: '/product/:id',
+    name: 'Product',
+    component: () => import ('../views/ProductView.vue')
+  },
+
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: () => import ('../views/CartView.vue')
+  },
+
+  {
+    path: '/checkout',
+    name: 'Checkout',
+    component: () => import ('../views/CheckoutView.vue')
+  },
+
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: () => import ('../views/NotFoundView.vue')
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes
 })
 
 export default router
---
+++ src/views/CatalogView.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>Catalog Page</div>
+</template>
